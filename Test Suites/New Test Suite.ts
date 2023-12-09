10bee3bcf04cf41e7e84ceeae11ec85d0cb1d0f8
pranay <pranaylukka@github.com>
2023-12-10 01:23:18 +0800

    profiles

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -11,11 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>94b75925-f440-42a2-a07f-99e548c5203c</testSuiteGuid>
-   <testCaseLink>
-      <guid>029da904-3ea1-424e-9562-b6e102083294</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/z_last</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
